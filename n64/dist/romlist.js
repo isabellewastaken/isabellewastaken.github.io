f09f2c98757471da5a70751263d6dd988573f3e1
Isabelle <progamer.charlie@icloud.com>
2026-05-22 06:53:41 +0100

    May Update 3

--- n64/dist/romlist.js
+++ n64/dist/romlist.js
@@ -6,7 +6,7 @@ var ROMLIST = [
     {url:"roms/smash.z64", title:"Super Smash Bros"},
     {url:"roms/banjo.z64", title:"Banjo-Kazooie"},
     {url:"roms/majora.z64", title:"Majora's Mask"},
-    {url:"roms/mariop3.z64", title:"Mario Party 3"},
+    //{url:"roms/mariop3.z64", title:"Mario Party 3"},
     {url:"roms/paper_mario.z64", title:"Paper Mario"},
     {url:"roms/dk64.z64", title:"Donkey Kong 64"},
 ];
